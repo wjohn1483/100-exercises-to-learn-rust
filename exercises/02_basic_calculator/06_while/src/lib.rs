38546656875e0decb6de848487b8b2752a2ff9da
Chia-Hung Wan <chiahung.wan@yahooinc.com>
2024-07-01 17:10:54 +0800

    Exercise

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,14 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    // todo!()
+    let mut result = 1;
+    let mut n = n;
+    while n > 0 {
+        result *= n;
+        n -= 1;
+    }
+    result
 }
 
 #[cfg(test)]
